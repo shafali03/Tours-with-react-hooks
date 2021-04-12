776a9ea24b292509f3dbda271467b0bf6193f375
shafali03 <shaf03sm@gmail.com>
2021-04-12 09:58:23 +0100

    Fetching data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import Loading from "./components/Loading/Loading";
+import Tours from "./components/Tours/Tours";
+
+const url = "https://course-api.netlify.app/api/react-tour-project";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
   return (
-    <div className="App">
-      <h1>test</h1>
-    </div>
+    <main>
+      <Tours />
+    </main>
   );
 }
 
